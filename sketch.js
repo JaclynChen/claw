c3638f022f7eb173ffae6355354b19eecdf986a1
Jaclyn Chen <jaclyn.chen5@gmail.com>
2024-05-17 17:02:59 -0700

    initial ender3 movement

---
+++ sketch.js
@@ -0,0 +1,83 @@
+let fab;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    fab = createFab();
+  
+    let connectButton = createButton('connect!');
+    connectButton.position(20, 20);
+    connectButton.mousePressed(function() {
+      fab.serial.requestPort(); // choose the serial port to connect to
+    });
+
+    let printButton = createButton('print!');
+    printButton.position(20, 60);
+    printButton.mousePressed(function() {
+      fab.print(); // start streaming the commands to printer
+    });
+
+    let stopButton = createButton('stop!');
+    stopButton.position(20, 100);
+    stopButton.mousePressed(function() {
+      fab.stopPrint(); // stop streaming the commands to printer
+    });
+}
+
+// function fabDraw() {
+//   // setup!
+//   fab.setAbsolutePosition(); // set all axes (x.y/z/extruder) to absolute
+//   fab.setERelative(); // put extruder in relative mode, independent of other axes
+//   fab.autoHome();
+//   // fab.setNozzleTemp(205); // °C - you should use a temperature best suited for your filament!
+//   // fab.setBedTemp(60); // °C - best temperature for good adhesion/no curling will vary based on filament used!
+//   // fab.introLine(0.2); // draw to lines on the left side of the print bed
+  
+//   // variables for our hollow cube!
+//   let sideLength = 20; //mm
+//   let x = 100; 
+//   let y = 100;
+//   let speed = 10; // mm/sec
+//   let layerHeight = 0.2; // mm
+
+//   // design our hollow cube!
+//   fab.moveRetract(x, y, layerHeight); // move to the start (x,y,z) position without extruding
+
+// //   fab.moveRetract(100, 100, 100);
+
+
+//   for (let z = layerHeight; z <= sideLength; z += layerHeight) { 
+//     if (z == layerHeight) { // if it's the first layer
+//     speed = 10; // slow print speeed down for the first layer
+//     }
+//     else {
+//       speed = 25;
+//     }
+//     fab.moveExtrude(x + sideLength, y, z, speed); // move along the bottom side while extruding filament
+//     fab.moveExtrude(x + sideLength, y + sideLength, z, speed); // right side
+//     fab.moveExtrude(x, y + sideLength, z, speed); // top side
+//     fab.moveExtrude(x, y, z, speed); //left side
+//   }
+
+//   fab.presentPart();
+//   fab.render();
+// }
+
+function draw() {
+  orbitControl(2, 2, 0.1);
+  background(255);
+  fab.render();
+
+  // console.log("draw loop!");
+}
+
+function fabDraw() {
+    console.log("starting fabDraw");
+    // Set the machine up for printing
+    fab.setAbsolutePosition();
+    fab.setERelative();
+    fab.autoHome();
+    fab.setTemps(205, 60);
+ 
+    // Now move around!
+    fab.move(100, 100, 0.2); // (x, y, z)
+}
